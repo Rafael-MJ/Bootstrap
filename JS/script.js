ba3992fe2699254c5accb67a1eae35a554e16080
Rafael Modena Jakubovsky <rafaeljakubovsky@gmail.com>
2023-10-13 23:26:10 -0300

    organized files

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
 function clickBuy(param) {
   window.alert("Sucesso! Você adquiriu o curso de " + param + "!");
-  window.location.href = "../sign-in/index.html";
+  window.location.href = "../[1] sign-in/index.html";
 }
